18f684a6b978683b089f1cb4c95799dc9cc2659a
golok_ku <49714008+GOLOKKU@users.noreply.github.com>
2022-08-12 21:16:03 +0700

    tugas ha te em el dan pe ha pe

---
+++ XI/tugas_laporan/dasa_darma.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<title>Dasa Darma</title>
+
+<body>
+    <br>
+    <h1 align="center">DASA DARMA</h1><img src="https://cdn.discordapp.com/attachments/885019575484313640/1004543314747473971/unknown.png" align="right" weight="250" height="250">
+    <?php
+    $dasa_darma = [
+        "1. Takwa kepada tuhan yang maha esa",
+        "2. Cinta alam dan kasih sayang sesama manusia",
+        "3. Patriot yang sopan dan kesatria",
+        "4. Patuh dan suka bermusyawarah",
+        "5. Rela menolong dan tabah",
+        "6. Rajin, terampil, dan gembira",
+        "7. hemat, cermat, dan bersahaja",
+        "8. Disiplin, berani, dan setia", 
+        "9. Bertanggung jawab dan dapat dipercaya",
+        "10.Suci dalam pikiran, perkataan, dan perbuatan"
+    ];
+    foreach ($dasa_darma as $text) {
+        echo "<h3> $text </h3>";
+    };
+    ?>
+
+</body>
+
+</html>
---
+++ XI/tugas_laporan/tanggal.php
@@ -0,0 +1,42 @@
+<div>
+    <table width="50%">
+        <tr>
+            <td style="text-decoration: underline; text-align: left">Rabiul Awal 1440h</td>
+            <td style="text-align: center">
+                <h1 style="color: red">Januari 2022</h1>
+            </td>
+            <td style="text-decoration: underline;text-align: right">Bakda Mulud 1552h</td>
+        </tr>
+        <tr>
+            <td style="text-align: left">Jumadil Awal 1440h</td>
+            <td style="text-align: center">
+            </td>
+            <td style="text-align: right">Jumadil Mulud 1552h</td>
+        </tr>
+    </table>
+    <table border="1" width="50%" style="text-align: center">
+        <tr style="background-color: blue">
+            <th style="background-color: red">Minggu</th>
+            <th>Senin</th>
+            <th>Selasa</th>
+            <th>Rabu</th>
+            <th>Kamis</th>
+            <th>Jumat</th>
+            <th>Sabtu</th>
+        </tr>
+        <?php
+        $start = 30;
+        $baris = 4;
+        $kolom = 6;
+        for ($c = 0; $c <= $baris; $c++) {
+            echo "<tr>";
+            for ($x = 0; $x <= $kolom; $x++) {
+                echo "<td>$start</td>";
+                $start += 1;
+                if ($start == 32) {
+                    $start = 1;
+                }
+            }
+            echo "</tr>";
+        }
+        ?>
